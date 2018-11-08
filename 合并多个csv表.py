547513f3d8b92c5afad1c2ad50cbab22c8f67d3f
little forest <447193772@qq.com>
2018-11-08 21:51:49 +0800

    优化

--- 合并多个csv表.py
+++ 合并多个csv表.py
@@ -2,17 +2,16 @@ import pandas as pd
 import numpy as np
 import os
 
-path_in = r"C:\Users\x\Desktop\csvS"
+path = r"C:\Users\x\Desktop\pandas操作原数据\2、合并多个csv表-原数据"
 
-list = os.listdir(path_in)
-os.chdir(path_in)
 li = []
-for filename in list:
+for filename in os.listdir(path):
     if os.path.splitext(filename)[1] == '.csv':
-        df = pd.read_csv(filename, engine='python')
+        os.chdir(path)
+        df = pd.read_csv(filename,engine='python')
         li.append(df)
 
-#axis默认为0
+#默认axis=0
 dfs = pd.concat(li,sort=False)
-dfs.to_csv(path_in+'\\合并.csv',index=False,encoding='utf_8_sig')
+dfs.to_csv(path+'//result.csv',index=False,encoding='utf_8_sig')
 
